16fb4a35866df79cf4a6ff07654e8b3a5dd386e1
Andrew Edwards <andrew-edwards@live.com>
2016-05-24 12:44:36 +0800

    trying email functionality

--- templates/email.php
+++ templates/email.php
@@ -1,25 +1,89 @@
+<html>
+<head><title>Your Email Has Been Sent</title></head>
+<body>
+
+
+<ul>
 <?php
-//if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
+        ### Replace the email address with your own, 
+        ### don't forget the escape backslash before the @ symble.
+        $to = "andrew-edwards\@live.com";
+        
+        ### Replace the subject line with your own.
+        $subject = "Question Regarding Website";
+        
+        ### Placeholder for Email Body
+        $message = "";    
+        
+        ### Placeholder for HTML Feedback
+        $html_message = "";
+        
 
-  //Email information
-  $admin_email = "andrew-edwards@live.com";
-  $first_name = $_REQUEST['first_name'];
-  $last_name = $_REQUEST['last_name'];
-  $email = $_REQUEST['email'];
-  $comment = $_REQUEST['comment'];
+        ### Find out what the request method was (We support POST and GET methods)
 
-  //send email
-  mail($admin_email, "$first_name", '$last_name', $comment, "From:" . $email);
+        $request_method = $_SERVER["REQUEST_METHOD"];
 
-  //Email response
-  echo "Thank you for contacting us!";
-  }
+		
+        if ($request_method == "GET")                                   ### Info was passed using GET
+                {
+                
+                ### Tell them
+                foreach (array_keys($_GET) as $key)                     ### Find out each variable name
+                        {
+                        $pair = "";										### Place holder for the current key-value pair
+		                $pair = $pair."$key";                           ### Variable name
+		                $pair = $pair.": ".$_GET[$key]."\n";		    ### Variable value
+		                
+                        $message = $message.$pair ; 			### Construct email body
+                        
+                        $html_message = $pair."<br/>";			### Blank line makes things for HTML page	
+                        print $html_message;
+                        }
+                        
+                print "</ul>\n";
+                
+                               }
 
-  //if "email" variable is not filled out, display the form
-  else  {
+        elseif ($request_method == "POST")                              ### Repeat the same proceedure for POST
+                {
+                	
+                  foreach (array_keys($_POST) as $key)
+                        {
+                        $pair = "";
+		                $pair = $pair."$key";                           ### Variable name
+		                $pair = $pair.": ".$_POST[$key]."\n";			### Variable value
+		                
+                        $message = $message.$pair ; 			### Construct email body
+                        
+                        $html_message = $pair."<br/>";			### Blank line makes things for HTML page	
+                        print $html_message;
+                        }
+                        
+                print "</ul>\n";
+                
+                }
+        else                                                            ### If we get here, some unknown method was used
+                {
+                print "Unsupported method used\n";
+                }		
+                
+                if ($message == ""){
+                	print "<h3>Please enter a valid message.</h3>";
+					exit;
+				}
+		
+		### Sending Email using the PHP mail function.
+		$sent = mail($to, $subject, $message);
+		if ($sent){
+			print "<H3>Your email has been sent.</h3>\n";
+		}else{
+			print "<H3>There were some problem sending this email. Please contact me directly at andrew@andrewedwards.com.au</h3>\n";
+		}
+		
+
 ?>
-echo '<script type="text/javascript">alert("shits fucked.");</script>';
-<?php
-  }
-?>
+
+</ul>
+</body>
+</html>
+
