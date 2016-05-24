9b4e58a63a3acead8ceaaf9b8e0de15e0ff74262
Andrew Edwards <andrew-edwards@live.com>
2016-05-24 12:27:49 +0800

    trying email functionality

--- templates/email.php
+++ templates/email.php
@@ -3,7 +3,7 @@
   if (isset($_REQUEST['email']))  {
 
   //Email information
-  $admin_email = "someone@example.com";
+  $admin_email = "andrew-edwards@live.com";
   $first_name = $_REQUEST['first_name'];
   $last_name = $_REQUEST['last_name'];
   $email = $_REQUEST['email'];
